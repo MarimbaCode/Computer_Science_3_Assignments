896cbb3c37a0fec81cfcf23a3c80ca4a799ce15b
dearaujo_882564 <stefan.dearaujo64@k12.leanderisd.org>
2021-09-16 12:38:41 -0500

    Initial commit

---
+++ src/net/thejrdev/assignments/lfsr/LFSR.java
@@ -0,0 +1,50 @@
+package net.thejrdev.assignments.lfsr;
+
+public class LFSR {
+
+    protected int state;
+    protected int length;
+    protected int tap;
+
+
+    public LFSR(int initialState, int length, int tap){
+        this.state = initialState;
+        this.length = length;
+        this.tap = tap;
+    }
+
+
+
+
+    private int shift(){
+        int bit = getBit(length-1);
+        int tapBit = getBit(tap);
+        state <<= 1;
+        state &= (1<<length) -1;
+        state |= bit^tapBit;
+        return bit ^ tapBit;
+    }
+
+    private int getBit(int loc){
+        return (state >> loc) & 1;
+    }
+
+    public long getByte(){
+        return getNumber(8);
+    }
+
+    public long getNumber(int bits){
+        long res = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bits; i++) {
+            System.out.println(Integer.toBinaryString(state));
+            res <<= 1;
+            int bit = shift();
+            res += bit;
+            sb.append(bit);
+        }
+        System.out.println(sb.toString());
+        return res;
+    }
+
+}
---
+++ src/net/thejrdev/assignments/lfsr/LFSR_Main.java
@@ -0,0 +1,17 @@
+package net.thejrdev.assignments.lfsr;
+
+public class LFSR_Main {
+
+    public static void main(String[] args) {
+
+        LFSR lfsr = new LFSR(0b11001001001, 11, 8);
+
+        System.out.println(Long.toBinaryString(lfsr.getNumber(37)));
+        System.out.println();
+        System.out.println("1110110111001011010111001100010111111");
+
+    }
+}
+//1001001001101111110110110100100100100
+
+//1110110111001011010111001100010111111
